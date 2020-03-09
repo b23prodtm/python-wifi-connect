687fccd166acf1698612d6288b2e4e3a3e4ca5ad
Tiana Rakoto Arimanana <5566338+b23prodtm@users.noreply.github.com>
2020-03-09 22:44:41 +0100

    Merge development

--- nm_scripts/start_dnsmasq.sh
+++ nm_scripts/start_dnsmasq.sh
@@ -8,7 +8,7 @@ fi
 
 DEFAULT_GATEWAY="192.168.42.1"
 DEFAULT_DHCP_RANGE="192.168.42.2,192.168.42.254"
-DEFAULT_INTERFACE="wlan0" # use 'ip link show' to see list of interfaces
+DEFAULT_INTERFACE=${DEFAULT_INTERFACE:-"wlan0"} # use 'ip link show' to see list of interfaces
 
 # run dnsmasq in the background
 CMD="dnsmasq"\
@@ -19,10 +19,7 @@ CMD="dnsmasq"\
 " --keep-in-foreground"\
 " --bind-interfaces"\
 " --except-interface=lo"\
-" --conf-file"\
-" --no-hosts" 
+" --no-hosts"
 
 echo "Running in background: $CMD"
 `$CMD` &
-
-
